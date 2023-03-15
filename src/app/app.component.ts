0717081d195fa94f70061d759efa6a20113afcb1
ellizeurs <ellizeurs@gmail.com>
2023-03-15 08:51:04 -0300

    Update app.component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'convem_challenge';
+  title = 'Challenge - Convem';
+
+  submitResponse() {
+    alert('clicou')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromotionalAnswersComponent } from './promotional-answers/promotional-answers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromotionalAnswersComponent
   ],
   imports: [
     BrowserModule,
