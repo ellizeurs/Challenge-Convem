30812384cf62854ea5737dd4d8f17d60170ee3bd
ellizeurs <ellizeurs@gmail.com>
2023-03-26 14:30:04 -0300

    Update server request and response

--- src/app/promotional-form/promotional-form.component.ts
+++ src/app/promotional-form/promotional-form.component.ts
@@ -3,19 +3,19 @@ import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-promotional-form',
   templateUrl: './promotional-form.component.html',
   styleUrls: ['./promotional-form.component.css']
 })
 
+
 @Injectable()
 export class PromotionalFormComponent {
   constructor(private http: HttpClient) { }
-  userResponse : string = ''; // Start user response
-  buttonText : string = 'Enviar'; // Start button text
-  send : boolean = false; // Submitted form
+  userResponse: string = ''; // Start user response
+  buttonText: string = 'Enviar'; // Start button text
+  send: boolean = false; // Submitted form
 
   submitResponse() {
     if (this.userResponse == '') { //Check empty field
@@ -26,20 +26,20 @@ export class PromotionalFormComponent {
     this.buttonText = 'Enviando...';
     this.send = true; // Block button and field read only
     // Send server request
-    this.http.get('https://641077cd7b24bb91f21e25e1.mockapi.io/promotional_response/' + this.userResponse.toUpperCase()).subscribe(
-      data => { // Sucess request (/:id == SIM || /:id is empty)
-        if (Array.isArray(data)) // Empty field (second check)
-          this.buttonText = 'Erro';
-        else // Sucess
-          this.buttonText = 'Você está mais próximo de se juntar ao time!';
-      },
-      error => { //Error request (/:id != SIM)
-        this.buttonText = 'Erro';
-      }
-    );
+    this.http.post(
+      'http://localhost:3000',
+      { userResponse: this.userResponse })
+      .subscribe(
+        (data: any) => {
+          this.buttonText = data.message;
+        },
+        error => {
+          this.buttonText = "Erro";
+        }
+      );
   }
 
-  resetForm(){ // On keydown event in userResponse field restart the form
+  resetForm() { // On keydown event in userResponse field restart the form
     this.buttonText = 'Enviar';
     this.send = false;
   }
